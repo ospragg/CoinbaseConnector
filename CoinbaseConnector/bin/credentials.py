d0f88ca5b8ca84d086f7ae2d6f307e9027e5feea
Oliver Spragg <oliver@converge.io>
2019-01-21 14:54:56 +0100

    moved keys

---
+++ CoinbaseConnector/bin/credentials.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+api_key = "a2ea00700c9d0f1929bec91dd1facefd"
+api_secret = "nQ9Lm7XeFJnejaYctFcj31INH9IU2dVWMx9xvxPuYetH7v7c8svQISX+lG9OwIyXGbh11vyQwqsDdKapX0zVJQ=="
+api_passphrase = "5jgxgptgky6"
--- CoinbaseConnector/bin/main.py
+++ CoinbaseConnector/bin/main.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append('../')
 
 import time
+import credentials
 
 from coinbaseconnector.cc_websocket import Websocket
 from coinbaseconnector.cc_connector import Connector
@@ -13,13 +14,14 @@ url="https://api-public.sandbox.pro.coinbase.com"
 #url = "wss://ws-feed-public.sandbox.pro.coinbase.com"
 url = "wss://ws-feed.pro.coinbase.com/"
 
-"""
+
 cc = Connector(rest_url="https://api-public.sandbox.pro.coinbase.com",
 				 websocket_url="wss://ws-feed-public.sandbox.pro.coinbase.com",
 				 product="BTC-USD",
-				 api_key="a2ea00700c9d0f1929bec91dd1facefd",
-				 api_secret="nQ9Lm7XeFJnejaYctFcj31INH9IU2dVWMx9xvxPuYetH7v7c8svQISX+lG9OwIyXGbh11vyQwqsDdKapX0zVJQ==",
-				 api_passphrase="5jgxgptgky6")
+				 api_key=credentials.api_key,
+				 api_secret=credentials.api_secret,
+				 api_passphrase=credentials.api_passphrase)
+
 """
 cc = Connector(rest_url="https://api-public.sandbox.pro.coinbase.com",
 				 #websocket_url="wss://ws-feed-public.sandbox.pro.coinbase.com",
@@ -28,7 +30,7 @@ cc = Connector(rest_url="https://api-public.sandbox.pro.coinbase.com",
 				 api_key="",
 				 api_secret="",
 				 api_passphrase="")
-
+"""
 cc.start()
 
 for i in xrange(0, 10):
